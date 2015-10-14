272b23ed8e2c922f902fe3a51547193bbdba5dd7
Tim Kelly <tim.kelly@bazaarvoice.com>
2015-10-14 14:57:31 -0500

    commit 3

--- adaptiveLayoutTut/AdaptiveWeather/AdaptiveWeatherTests/AdaptiveWeatherTests.swift
+++ adaptiveLayoutTut/AdaptiveWeather/AdaptiveWeatherTests/AdaptiveWeatherTests.swift
@@ -3,7 +3,7 @@
 //  AdaptiveWeatherTests
 //
 //  Created by Tim Kelly on 8/28/15.
-//  Copyright (c) 2015-2016 Fizzy Artwerks. All rights reserved.
+//  Copyright (c) 2015-2016 FizzyArtwerks. All rights reserved.
 //
 
 
